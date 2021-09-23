9bb4e330bfb6fe9af594bc20cee3fd37a672d684
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-23 11:13:38 -0500

    renamed

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -5,21 +5,7 @@ async function newFormHandler(event) {
   const post_url = document.querySelector('input[name="post-url"]').value;
 
   const response = await fetch(`/api/posts`, {
-    method: 'POST',
-    body: JSON.stringify({
-      title,
-      post_url
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert(response.statusText);
-  }
+    
+})
 }
-
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
