a25f90423cf62f9c857abdb339bac5b257120479
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-10-05 04:33:48 -0500

    home-routes complete

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,48 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const router = require ("express").Router();
+const _require = require ("../models"),
+    Post = _require.Post,
+    Comment = _require.Comment,
+    User = _require.User;
 
-// get all posts for homepage
+router.get("/", function (req, res) { Post.findAll ({include: [User]})
+  .then (function (dbPostData) {
+const posts = dbPostData.map (function (post) {
+      return post.get ({ plain: true });
+    });
+    res.render("all-posts", { posts: posts });
+  })["catch"](function (err) {
+    res.status(500).json(err);
+  });
+});
 
-// get single post
+router.get("/post/:id", function (req, res) {
+  Post.findByPk(req.params.id, {
+    include: [User, { model: Comment, include: [User]}]})
+    .then(function (dbPostData) {
+    if (dbPostData) {
+      const post = dbPostData.get ({
+        plain: true
+      });
+      res.render("single-post", {post: post});
+    } else { res.status(404).end()}})
+    ["catch"] (function (err) {
+    res.status(500).json(err);
+  });
+});
 
+router.get("/login", function (req, res) {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return; 
+  } res.render("login");
+});
+
+router.get("/signup", function (req, res) {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  } res.render("signup");
+});
 
 module.exports = router;
+
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require ('express').Router();
 
-const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const dashboardRoutes = require ('./dashboard-routes.js');
+const apiRoutes = require ('./api/');
+const homeRoutes = require ('./home-routes.js');
 
-
+router.use ('/dashboard', dashboardRoutes);
+router.use ('/api', apiRoutes);
+router.use ('/', homeRoutes);
 
 module.exports = router;
